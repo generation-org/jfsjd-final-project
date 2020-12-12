3b0803086928f5c6ab04e70b3e3edef42a665160
Santiago Carrillo <sancarbar@gmail.com>
2020-12-12 11:30:54 -0500

    Update index.js

--- task-4/example/js/index.js
+++ task-4/example/js/index.js
@@ -1,36 +1,31 @@
 // Initialize a new TaskManager with currentId set to 0
-const taskManager = new TaskManager(0);
+const itemsController = new ItemsController(0);
 
 // Select the New Task Form
-const newTaskForm = document.querySelector('#newTaskForm');
+const newItemForm = document.querySelector('#newItemForm');
 
 // Add an 'onsubmit' event listener
-newTaskForm.addEventListener('submit', (event) => {
+newItemForm.addEventListener('submit', (event) => {
     // Prevent default action
     event.preventDefault();
 
     // Select the inputs
-    const newTaskNameInput = document.querySelector('#newTaskNameInput');
-    const newTaskDescription = document.querySelector('#newTaskDescription');
-    const newTaskAssignedTo = document.querySelector('#newTaskAssignedTo');
-    const newTaskDueDate = document.querySelector('#newTaskDueDate');
+    const newItemNameInput = document.querySelector('#newTaskNameInput');
+    const newItemDescription = document.querySelector('#newTaskDescription');
 
     /*
         Validation code here
     */
 
     // Get the values of the inputs
-    const name = newTaskNameInput.value;
-    const description = newTaskDescription.value;
-    const assignedTo = newTaskAssignedTo.value;
-    const dueDate = newTaskDueDate.value;
+    const name = newItemNameInput.value;
+    const description = newItemDescription.value;
+    const createdAt = new Date();
 
     // Add the task to the task manager
-    taskManager.addTask(name, description, assignedTo, dueDate);
+    itemsController.addTask(name, description, createdAt);
 
     // Clear the form
-    newTaskNameInput.value = '';
-    newTaskDescription.value = '';
-    newTaskAssignedTo.value = '';
-    newTaskDueDate.value = '';
+    newItemNameInput.value = '';
+    newItemDescription.value = '';
 });
